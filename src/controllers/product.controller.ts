8d161008596c103f76b9f4d9c3037a8d5aae5cc7
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-05-31 19:15:15 +0200

    refactor: mejorar mensajes y código respuesta y depurar endpoints productos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -13,31 +13,43 @@ export const getAllProducts = async (req: Request, res: Response) => {
 
 
 export const createProduct = async (req: Request, res: Response) => {
-    const result = await productService.createProduct(req.body);
-    if (result.success) {
-        res.status(201).json(result);
-    } else {
-        res.status(400).json(result);
+    try {
+        const result = await productService.createProduct(req.body);
+        if (result.success) {
+            res.status(201).json(result);
+        } else {
+            res.status(400).json(result);
+        }
+    } catch (err) {
+        res.status(500).json({ error: 'Error interno' });
     }
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
+    try {
     const id = req.params.id;       // ID del producto desde la URL
     const data = req.body;          // Datos a actualizar desde el body
     const result = await productService.updateProduct(id, data);
     if (result.success) {
-        res.status(201).json(result);
+        res.status(200).json(result);
     } else {
         res.status(400).json(result);
     }
+}catch (err) {
+    res.status(500).json({ error: 'Error interno' });
+}
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
+    try {
     const id = req.params.id;
     const result = await productService.deleteProduct(id);
-        if (result.success) {
-        res.status(201).json(result);
+    if (result.success) {
+        res.status(200).json(result);
     } else {
         res.status(400).json(result);
     }
+}catch (err) {
+    res.status(500).json({ error: 'Error interno' });
+}
 };
