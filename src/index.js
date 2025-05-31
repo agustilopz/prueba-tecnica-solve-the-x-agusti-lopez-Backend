ab896154a8b76a1ce9cace66e69c2f084cd52a92
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-05-31 12:08:34 +0200

    chore: estructura inicial con TypeScript y prueba básica

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Función de prueba
+function saludar(nombre) {
+    return `Hola, ${nombre}.`;
+}
+let saludo = saludar("Juan");
+console.log(saludo);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+// Función de prueba
+function saludar(nombre: string): string {
+    return `Hola, ${nombre}.`;
+}
+
+let saludo: string = saludar("Juan");
+
+console.log(saludo);
